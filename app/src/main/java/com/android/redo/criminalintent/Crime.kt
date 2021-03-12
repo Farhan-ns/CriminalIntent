8591aa7ed95e11e51191dff6f536de3f0fbf6f43
Farhanunnasih <farhanunnasih@gmail.com>
2021-03-12 22:47:31 +0700

    - Added Crime data class

---
+++ app/src/main/java/com/android/redo/criminalintent/Crime.kt
@@ -0,0 +1,9 @@
+package com.android.redo.criminalintent
+
+import java.util.*
+
+
+data class Crime(val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false)
