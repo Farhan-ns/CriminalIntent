f3b84b04c383a830fd4c868127aa04dcb52d1acc
Farhanunnasih <farhanunnasih@gmail.com>
2021-03-13 23:03:17 +0700

    Added CrimeFragment.kt (Fragment Controller)

--- app/src/main/java/com/android/redo/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/android/redo/criminalintent/CrimeFragment.kt
@@ -1,16 +1,39 @@
 package com.android.redo.criminalintent
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
     private lateinit var crime: Crime
+    private lateinit var edtTitleField: EditText
+    private lateinit var dateButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        edtTitleField = view.findViewById(R.id.edt_crime_title) as EditText
+        dateButton = view.findViewById(R.id.datepick_crime_date) as Button
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+
+        return view
     }
 }
